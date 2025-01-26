9fd8352d53dccc800edf5489f02101b0b4030ae4
Krutilin_Nikita <Krutilin.n@yandex.ru>
2025-01-27 00:09:01 +0300

    Добавлены реализована отправка комплиментов

---
+++ compliment_bot/chat_gpt.py
@@ -0,0 +1,27 @@
+import google.generativeai as genai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Загружает переменные из .env
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+def generate_compliment(text: str):
+    genai.configure(api_key=GEMINI_API_KEY)
+
+    try:
+        # Создание модели
+        model = genai.GenerativeModel("gemini-1.5-flash")
+
+        # Подготовка запроса
+        s1 = 'Действуй как умный сильный, независимый и уверенный мужчина.\n'
+        s2 = f'Напиши интригующий, изысканный и добрый комплимент для девушки, в которой тебе нравится {text}.\n'
+        s3 = '\nНапиши ответ более 30 символов и менее 50 символов. В конце задай естественный и логичный вопрос для легкого продолжения общения по теме комплимента'
+        prompt = s1 + s2 + s3
+
+        # Генерация текста
+        response = model.generate_content(prompt)
+        return response.text
+
+    except Exception as e:
+        return f"Произошла ошибка: {e}"
--- compliment_bot/main.py
+++ compliment_bot/main.py
@@ -1,7 +1,37 @@
 from dotenv import load_dotenv
 import os
+from chat_gpt import generate_compliment
+import telebot
 
 load_dotenv()  # Загружает переменные из .env
 
-SECRET_KEY = os.getenv("API_KEY")
-print(SECRET_KEY)
+# Токен Telegram-бота
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+bot = telebot.TeleBot(TELEGRAM_TOKEN)
+
+# Обработчик команды /start
+@bot.message_handler(commands=["start"])
+def start_message(message):
+    bot.send_message(
+        message.chat.id,
+        "Привет! Напиши, что тебе нравится в девушке, и я помогу придумать красивый комплимент."
+    )
+
+# Обработчик текстовых сообщений
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    user_input = message.text
+    try:
+        # Генерация комплимента
+        compliment = generate_compliment(user_input)
+        bot.send_message(message.chat.id, compliment)
+    except Exception as e:
+        print(f"Ошибка: {e}")
+        bot.send_message(
+            message.chat.id,
+            "Произошла ошибка при генерации комплимента. Попробуйте снова!"
+        )
+
+if __name__ == "__main__":
+    # Запуск бота
+    bot.polling()
